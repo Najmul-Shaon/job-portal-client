877d189786855d52bbd7ddc3377084c2d9c5abdf
Najmul shaon <najmul.nh.shaon@gmail.com>
2024-12-11 00:56:15 +0600

    img animated on home banner

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -1,13 +1,22 @@
 import { easeOut } from "motion";
 import { motion } from "motion/react";
+import teamPic from "../../assets/team.webp";
 const Banner = () => {
   return (
     <div className="hero bg-base-200 min-h-96">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="flex-1">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-            className="max-w-sm rounded-lg shadow-2xl"
+          <motion.img
+            src={teamPic}
+            animate={{ y: [50, 100, 50] }}
+            transition={{ duration: 10, repeat: Infinity }}
+            className="max-w-sm w-64 rounded-t-[40px] rounded-br-[40px] shadow-2xl border-l-4 border-b-4 border-blue-400"
+          />
+          <motion.img
+            src={teamPic}
+            animate={{ x: [100, 150, 100] }}
+            transition={{ duration: 10, delay: 5, repeat: Infinity }}
+            className="max-w-sm w-64 rounded-t-[40px] rounded-br-[40px] shadow-2xl border-l-4 border-b-4 border-blue-400"
           />
         </div>
         <div className="flex-1">
@@ -21,7 +30,14 @@ const Banner = () => {
             }}
             className="text-5xl font-bold"
           >
-            Latest Jobs For You!!
+            Latest{" "}
+            <motion.span
+              animate={{ color: ["orange", "pink"] }}
+              transition={{ duration: 2, delay: 1, repeat: Infinity }}
+            >
+              Jobs
+            </motion.span>{" "}
+            For You!!
           </motion.h1>
           <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
